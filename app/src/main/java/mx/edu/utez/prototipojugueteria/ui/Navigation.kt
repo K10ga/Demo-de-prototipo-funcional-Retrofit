331ba98318b48b1b263f3da021edbd3db9e30736
k10ga <20243ds093@utez.edu.mx>
2025-10-24 06:58:54 -0600

    preparado pero no al 100%

--- app/src/main/java/mx/edu/utez/prototipojugueteria/ui/Navigation.kt
+++ app/src/main/java/mx/edu/utez/prototipojugueteria/ui/Navigation.kt
@@ -1,2 +1,21 @@
 package mx.edu.utez.prototipojugueteria.ui
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import mx.edu.utez.prototipojugueteria.viewmodel.JugueteViewModel
+import mx.edu.utez.prototipojugueteria.viewmodel.LoginViewModel
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    val loginViewModel: LoginViewModel = viewModel()
+    val jugueteViewModel: JugueteViewModel = viewModel()
+
+    NavHots(navController = navController, startDestination = "login"){
+        composable("login"){
+
+
+        }
+    }
+
+}
