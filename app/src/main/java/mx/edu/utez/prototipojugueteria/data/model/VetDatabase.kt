73b1046ab2f6e92f9454b32d6593e5cf27c3acb8
k10ga <20243ds093@utez.edu.mx>
2025-11-04 17:46:21 -0600

    Lista la clase que une la entidate y el dao

---
+++ app/src/main/java/mx/edu/utez/prototipojugueteria/data/model/VetDatabase.kt
@@ -0,0 +1,9 @@
+package mx.edu.utez.prototipojugueteria.data.model
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Juguete::class], version = 1)
+abstract class VetDatabase : RoomDatabase(){
+    abstract fun JugueteDao(): JugueteDao
+}
