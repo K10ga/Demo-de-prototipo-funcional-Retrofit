6eab45e485230eb93b25601b1c0c60a1d0affa9c
k10ga <20243ds093@utez.edu.mx>
2025-11-04 17:42:33 -0600

    Solo modifique el tipo de dato de precio y meti lo demas para que sirviera la tabla

--- app/src/main/java/mx/edu/utez/prototipojugueteria/data/model/Juguete.kt
+++ app/src/main/java/mx/edu/utez/prototipojugueteria/data/model/Juguete.kt
@@ -1,11 +1,15 @@
 package mx.edu.utez.prototipojugueteria.data.model
 
 import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "juguetes")
 data class Juguete(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int=0,
     val nombreJuguete: String, //
     val tipoJuguete: String, //
-    val precio: Int, //
+    val precio: Float, //
     @DrawableRes val imagen: Int //
 )
