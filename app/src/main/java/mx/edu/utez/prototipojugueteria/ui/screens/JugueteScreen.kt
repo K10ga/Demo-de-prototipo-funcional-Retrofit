e6d5ab1c2faa0d8ded166fc6474ae4a1ee58793e
Carlos <20243ds110@utez.edu.mx>
2025-11-06 19:33:38 -0600

    Se modifico JugueteScreen.kt y se espera revision de correccion de errores

--- app/src/main/java/mx/edu/utez/prototipojugueteria/ui/screens/JugueteScreen.kt
+++ app/src/main/java/mx/edu/utez/prototipojugueteria/ui/screens/JugueteScreen.kt
@@ -2,9 +2,18 @@ package mx.edu.utez.prototipojugueteria.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -13,6 +22,39 @@ import mx.edu.utez.prototipojugueteria.viewmodel.JugueteViewModel
 import mx.edu.utez.prototipojugueteria.ui.components.texts.Title
 import mx.edu.utez.prototipojugueteria.ui.components.texts.Label
 
+
+@Composable
+fun JugueteScreen(viewModel: JugueteViewModel){
+
+    val juguetes by viewModel.JugueteUiState.collectAsState()
+    var id by remember { mutableStateOf("") }
+    var nombrejuguete by remember {mutableStateOf("")}
+    var tipojuguete by remember { mutableStateOf("") }
+    var precio by remember {mutableStateOf("")}
+
+    Column (modifier = Modifier.fillMaxSize()){
+        LazyColumn (modifier = Modifier.weight(1f)){
+            TextField(value = id, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
+            TextField(value = nombrejuguete, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
+            TextField(value = tipojuguete, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
+            TextField(value = precio, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
+
+            Button(onClick = {
+                viewModel.addNewJuguete(id,nombrejuguete,tipojuguete,precio)
+                id=""
+                nombrejuguete=""
+                tipojuguete=""
+                precio=""
+            }) {
+                Text("Guardar Juguete")
+            }
+        }
+    }
+}
+
+
+
+/*
 @Composable
 fun JugueteScreen(viewModel: JugueteViewModel, navController: NavController){
     Column (
@@ -30,3 +72,4 @@ fun JugueteScreen(viewModel: JugueteViewModel, navController: NavController){
         Label("No hay más pasaportes")
     }
 }
+*/
