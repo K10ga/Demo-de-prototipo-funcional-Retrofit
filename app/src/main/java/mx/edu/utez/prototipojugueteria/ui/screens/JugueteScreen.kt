4d3778fc1bf8cb02eca5537f5a45801123531635
k10ga <20243ds093@utez.edu.mx>
2025-11-06 22:50:15 -0600

    corregi unos errores de carlitos

--- app/src/main/java/mx/edu/utez/prototipojugueteria/ui/screens/JugueteScreen.kt
+++ app/src/main/java/mx/edu/utez/prototipojugueteria/ui/screens/JugueteScreen.kt
@@ -1,9 +1,9 @@
 package mx.edu.utez.prototipojugueteria.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
+
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -15,66 +15,54 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.*
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavController
 import mx.edu.utez.prototipojugueteria.viewmodel.JugueteViewModel
-import mx.edu.utez.prototipojugueteria.ui.components.texts.Title
-import mx.edu.utez.prototipojugueteria.ui.components.texts.Label
+import androidx.compose.foundation.lazy.items
 
 
 @Composable
 fun JugueteScreen(viewModel: JugueteViewModel){
 
+    // Estado para la lista de juguetes
     val juguetes by viewModel.JugueteUiState.collectAsState()
-    var id by remember { mutableStateOf("") }
-    var nombrejuguete by remember {mutableStateOf("")}
-    var tipojuguete by remember { mutableStateOf("") }
-    var precio by remember {mutableStateOf("")}
 
-    Column (modifier = Modifier.fillMaxSize()){
+
+    var nombreJuguete by remember { mutableStateOf("") }
+    var tipoJuguete by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var imagen by remember { mutableStateOf("") }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+
+    ){
+        // Muestra la lista de juguetes
         LazyColumn (modifier = Modifier.weight(1f)){
-
-            TextField(value = id, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
-
-            TextField(value = nombrejuguete, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
-
-            TextField(value = tipojuguete, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
-
-            TextField(value = precio, onValueChange = {id = it}, label = {Text("ID")}, singleLine = true)
-
-            Button(onClick = {
-                viewModel.addNewJuguete(id,nombrejuguete,tipojuguete,precio)
-                id=""
-                nombrejuguete=""
-                tipojuguete=""
-                precio=""
-            }) {
-                Text("Guardar Juguete")
+            items(juguetes){ juguete ->
+                Text(text = " ${juguete.nombreJuguete} (${juguete.tipoJuguete}) - $${juguete.precio}")
             }
         }
+
+
+
+        TextField(value = nombreJuguete, onValueChange = {nombreJuguete = it}, label = {Text("nombreJuguete")}, singleLine = true)
+
+        TextField(value = tipoJuguete, onValueChange = {tipoJuguete = it}, label = {Text("tipoJuguete")}, singleLine = true)
+
+        TextField(value = precio, onValueChange = {precio = it}, label = {Text("precio")}, singleLine = true)
+
+        Button(onClick = {
+            val precioFloat = precio.toFloatOrNull() ?: 0f
+            val imagenInt = imagen.toIntOrNull() ?: 0
+            viewModel.addNewJuguete(nombreJuguete, tipoJuguete,precioFloat, imagenInt)
+            nombreJuguete =""
+            tipoJuguete=""
+            precio=""
+            imagen=""
+        }) {
+            Text("Guardar Juguete")
+        }
+
+
     }
 }
-
-
-
-/*
-@Composable
-fun JugueteScreen(viewModel: JugueteViewModel, navController: NavController){
-    Column (
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.padding(30.dp)
-    ) {
-        Title("Jueguetes registrados")
-        //esto puede que uziel o carlitos tenga que encargarse del jugueteList para que esta parte funcione
-        /*
-        val jueguetes by viewModel.juguetes.collectAsStateWithLifecycle()
-        JueguetesList(jueguetes) { jueguete ->
-            viewModel.clickJueguete(jueguetes)
-            navController.navigate("")
-        }*/
-        Label("No hay más pasaportes")
-    }
-}
-*/
